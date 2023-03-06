a1212bb95c489e6d315f8b325a6fafe0029aa3f2
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2023-03-06 02:02:05 +0000

    init

---
+++ tfquizcomp/src/Component/Quiz.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image"
+
+export default function Quiz(){
+    
+    return (
+        <>
+            <div className="justify-center">
+                <div className="text-center">問題文がうんたらかんたら</div>
+                <div className="flex place-items-center content-center place-content-center">
+                    <div className="flex-1 place-self-center">
+                        <h2>正しい</h2>
+                        <div>
+                            ここに○を置く
+                            <Image src="/technology_blue_circle.png" width={75} height={75} alt="true"/>
+                        </div>
+                    </div>
+                    <div className="flex-1 place-self-center">
+                        <h2>正しくない</h2>
+                        <div>ここに×を置く</div>
+                    </div>
+                </div>
+            </div>            
+        </>
+    )
+}
