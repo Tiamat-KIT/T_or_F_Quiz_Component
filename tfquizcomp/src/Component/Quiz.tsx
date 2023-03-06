25d53a7acae5eda265a4ddbb6d2ca9bd6fd73672
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2023-03-06 03:46:34 +0000

    ComponentSetting

--- tfquizcomp/src/Component/Quiz.tsx
+++ tfquizcomp/src/Component/Quiz.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import React from "react"
 
 export default function Quiz(){
     function IconButton({ImageLink,alt_str}:{ImageLink:string,alt_str:string}){
@@ -10,33 +11,57 @@ export default function Quiz(){
             </button>
         )
     }
+
+    function FlexItem({children}: {children:React.ReactNode}){
+        return(
+            <div className="flex-1">
+                {children}
+            </div>
+        )
+    }
+    function Container({children}: {children: React.ReactNode}){
+        return(
+            <div className="container mx-auto p-5">
+                {children}
+            </div>
+        )
+    }
+    function Card({children}: {children: React.ReactNode}){
+        return(
+            <div className="card">
+                <div className="card-body card-bordered">
+                    {children}
+                </div>
+            </div>
+        )
+    }
+
+
     return (
         <>
-            <div className="container mx-auto p-5">
-                <div className="card">
-                    <div className="card-body card-bordered">
-                        <div className="text-center card-title">問題</div>
+            <Container>
+                <Card>
+                    <div className="text-center card-title">問題</div>
                         <figure>
                             <Image src="/testImage.jpg" width={500} height={250} alt="image" />
                         </figure>
                         <p>問題文が云々</p>
                         <div className="flex">
-                            <div className="flex-1 place-self-center">
+                            <FlexItem>
                                 <div className="flex">
                                     <h2>正しい</h2>
                                     <IconButton ImageLink="/technology_blue_circle.png" alt_str="true" />
                                 </div>
-                            </div>
-                            <div className="flex-1 place-self-center">
+                            </FlexItem>
+                            <FlexItem>
                                 <div className="flex">
                                     <h2>正しくない</h2>
                                     <IconButton ImageLink="/Batsu.png" alt_str="false" />
                                 </div>
-                            </div>
-                        </div>
+                            </FlexItem>
                     </div>
-                </div>  
-            </div>          
+                </Card>
+            </Container>          
         </>
     )
 }
