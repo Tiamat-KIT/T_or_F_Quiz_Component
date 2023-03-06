24cda8f5d50fd9a6e3b6566b91d8ad253757d272
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2023-03-06 03:36:11 +0000

    ComponentNest

--- tfquizcomp/src/Component/Quiz.tsx
+++ tfquizcomp/src/Component/Quiz.tsx
@@ -1,37 +1,41 @@
 import Image from "next/image"
 
 export default function Quiz(){
-    
+    function IconButton({ImageLink,alt_str}:{ImageLink:string,alt_str:string}){
+        return (
+            <button className="btn btn-square btn-ghost" type="submit">
+                <div>
+                    <Image src={ImageLink} width={100} height={100} alt={alt_str} />
+                </div>
+            </button>
+        )
+    }
     return (
         <>
             <div className="container mx-auto p-5">
-            <div className="card">
-                <div className="card-body card-bordered">
-                <div className="text-center card-title">問題</div>
-                <figure>
-                <Image src="/testImage.jpg" width={500} height={250} alt="image" />
-                </figure>
-                <p>問題文が云々</p>
-                <div className="flex">
-                    <div className="flex-1 place-self-center">
-                        <h2>正しい</h2>
-                        <button className="btn btn-square btn-ghost">
-                            <div>
-                                <Image src="/technology_blue_circle.png" width={75} height={75} alt="true" />
+                <div className="card">
+                    <div className="card-body card-bordered">
+                        <div className="text-center card-title">問題</div>
+                        <figure>
+                            <Image src="/testImage.jpg" width={500} height={250} alt="image" />
+                        </figure>
+                        <p>問題文が云々</p>
+                        <div className="flex">
+                            <div className="flex-1 place-self-center">
+                                <div className="flex">
+                                    <h2>正しい</h2>
+                                    <IconButton ImageLink="/technology_blue_circle.png" alt_str="true" />
+                                </div>
                             </div>
-                        </button>
-                    </div>
-                    <div className="flex-1 place-self-center">
-                        <h2>正しくない</h2>
-                        <button className="btn btn-square btn-ghost">
-                            <div>
-                                <Image src="/Batsu.png" width={75} height={75} alt="true" />
+                            <div className="flex-1 place-self-center">
+                                <div className="flex">
+                                    <h2>正しくない</h2>
+                                    <IconButton ImageLink="/Batsu.png" alt_str="false" />
+                                </div>
                             </div>
-                        </button>
+                        </div>
                     </div>
-                </div>
-                </div>
-            </div>  
+                </div>  
             </div>          
         </>
     )
