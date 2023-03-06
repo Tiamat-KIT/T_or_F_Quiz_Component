e4c44f306d374e7e32b8a85d64784c0dbb888c87
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2023-03-06 02:22:18 +0000

    btn_add

--- tfquizcomp/src/Component/Quiz.tsx
+++ tfquizcomp/src/Component/Quiz.tsx
@@ -9,10 +9,9 @@ export default function Quiz(){
                 <div className="flex place-items-center content-center place-content-center">
                     <div className="flex-1 place-self-center">
                         <h2>正しい</h2>
-                        <div>
-                            ここに○を置く
-                            <Image src="/technology_blue_circle.png" width={75} height={75} alt="true"/>
-                        </div>
+                        <button className="btn btn-ghost">
+                            <Image src="/technology_blue_circle.png" width={75} height={75} alt="true" />
+                        </button>
                     </div>
                     <div className="flex-1 place-self-center">
                         <h2>正しくない</h2>
